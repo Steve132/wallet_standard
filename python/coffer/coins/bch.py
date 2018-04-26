d48b816ced33cdae121a13eeb0b7a8cfd3b0c87a
Steve132 <steve1@soapforge.com>
2018-04-26 13:49:10 -0400

    Add bch cashaddr format

---
+++ python/coffer/coins/bch.py
@@ -0,0 +1,125 @@
+from ..wallet import *
+from _coin import *
+from _satoshicoin import *
+
+class CashAddrChecksumError(Exception):
+	def __init__(self,msg):
+		super(CashAddrChecksumError,self).__init__(msg)
+
+_base32chars="qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+class BCH(SatoshiCoin):
+	def __init__(self,is_testnet=False,cashaddr=False):
+		
+		if(not is_testnet):
+			bip32_prefix_private=0x0488ADE4
+			bip32_prefix_public=0x0488B21E
+			pkh_prefix=0x00
+			sh_prefix=0x05
+			wif_prefix=0x80
+		else:
+			bip32_prefix_private=0x04358394
+			bip32_prefix_public=0x043587CF
+			pkh_prefix=0x6F
+			sh_prefix=0xC4
+			wif_prefix=0xEF
+
+		sig_prefix=b'\x18Bitcoin Signed Message:\n'
+		
+		super(BCH,self).__init__('BCH',is_testnet=is_testnet,
+			bip32_prefix_private=bip32_prefix_private,
+			bip32_prefix_public=bip32_prefix_public,
+			pkh_prefix=pkh_prefix,
+			sh_prefix=sh_prefix,
+			wif_prefix=wif_prefix,
+			sig_prefix=sig_prefix)
+
+	def cashaddr_checksum(prefix,addrstring):
+		data=[ord(p) & 0x1F for p in prefix]
+		data+=[0]
+		data+=[_base32chars.index(a) for a in addrstring[:-8]]
+		data+=[0]*8
+		c=1
+		for d in data:
+			c0 = c >> 35
+			c = ((c & 0x07ffffffff) << 5) ^ d
+			if (c0 & 0x01):
+				c ^= 0x98f2bc8e61
+			if (c0 & 0x02):
+				c ^= 0x79b76d99e2
+			if (c0 & 0x04):
+				c ^= 0xf33e5fb3c4
+			if (c0 & 0x08):
+				c ^= 0xae2eabe2a8
+			if (c0 & 0x10):
+				c ^= 0x1e4f43e470
+		return _base.int2bytes(c ^ 1,5)
+
+	
+	#https://github.com/bitcoincashorg/spec/blob/master/cashaddr.md
+	def parse_cashaddr(self,addrstring):
+	
+		prefix=None
+		if(':' in addrstring):
+			prefix,addrstring=addrstring.split(':')
+		
+		byts=_base.baseX2bytes(addrstring,_base32chars)
+		version=ord(byts[0])
+		vsize=version & 0x07
+		vtype=(version & 0x78) >> 3
+		size=20+4*vsize
+		addrpayload=byts[1:-5]
+		checksum=byts[-5:]
+
+		
+		
+		possible_prefixes=["bitcoincash","bchtest","bchreg"]
+		if(prefix == None):
+			prefix="bchtest" if self.is_testnet else "bitcoincash"
+			
+		if(cashaddr_checksum(prefix,addrstring)!=checksum):
+			raise CashAddrChecksumError('CashAddr checksum failed for "%s"' % (addrstring))
+		
+		addrversions=[self.pkh_prefix,self.sh_prefix]
+		addrversion=addrversions[vtype]
+	
+		return bytes(chr(addrversion))+addrpayload
+	
+	def _write_cashaddr(self,abytes,prefix=None):
+		if(prefix==None):
+			if(self.is_testnet):
+				prefix='bchtest'
+			else:
+				prefix='bitcoincash'
+			
+		addrstring=''
+		csprefix=prefix if prefix != '' else 'bitcoincash'
+		addrpayload=abytes[1:]
+		addrversion=abytes[0]
+		vtype=[self.pkh_prefix,self.sh_prefix].index(addrversion)
+		vsize=(len(addrpayload)-20)//4
+		version=chr((vtype << 3) | vsize)
+		frontstr=b''+version+addrpayload+'\x00'*5
+		b32nocs=_base.bytes2baseX(frontstr,_base32chars)
+		checksum=cashaddr_checksum(csprefix,addrstring)
+		frontstr=b''+version+addrpayload+checksum
+		return _base.bytes2baseX(frontstr,_base32chars)
+
+	def pubkeys2addr(self,pubkeys,cashaddr=False,prefix=None,*args,**kwargs):
+		abytes=self.pubkeys2addr_bytes(pubkeys,*args,**kwargs)
+		if(cashaddr):
+			return self._write_cashaddr(abytes,prefix)
+		else:
+			return _base.bytes2base58c(abytes)
+
+	def parse_addr(self,addrstring)
+		if(':' in addrstring):
+			return self.parse_cashaddr(addrstring)
+		try:
+			return self.parse_cashaddr(addrstring)
+		except Exception as caerr:
+			try:
+				return _base.base58c2bytes(addrstring)
+			except Exception as err:
+				raise Exception("Could not parse BCH address: %r,%r" % (err.msg,caerr.msg))
+
+	
---
+++ python/coffer/key.py
@@ -0,0 +1,32 @@
+import hashlib,hmac
+from binascii import hexlify,unhexlify
+import struct
+import _crypto
+import _base
+		
+class PublicKey(object):
+	def __init__(self,pubkeydata,is_compressed=None):
+		self.pubkeydata=pubkeydata
+		self.is_compressed=is_compressed
+	def __add__(self,o):
+		return PublicKey(_crypto.pubkey_add(self.pubkeydata,o.pubkeydata),is_compressed=self.is_compressed)
+
+#TODO CANNOT HANDLE UNCOMPRESSED
+
+class PrivateKey(object):
+	def __init__(self,privkeydata,is_compressed=True):
+		self.privkeydata=privkeydata
+		self.is_compressed=is_compressed
+		if(not self.is_compressed):
+			raise Exception("Uncompressed private keys not implemented!")
+		if(not _crypto.privkey_verify(self.privkeydata)):
+			raise Exception("Invalid private key")
+
+	def pub(self):
+		pkd=_crypto.privkey_to_compressed_pubkey(self.privkeydata)
+		return PublicKey(pkd,is_compressed=True)
+
+	def __add__(self,o):
+		return PrivateKey(_crypto.privkey_add(self.privkeydata,o.privkeydata),is_compressed=self.is_compressed)
+	
+
